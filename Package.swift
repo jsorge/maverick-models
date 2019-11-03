a74bbc1802e83b5a853557c0c649d77ea652d3b1
Jared Sorge <jared@jsorge.net>
2019-11-03 08:30:52 -0800

    Update version

--- Package.swift
+++ Package.swift
@@ -1,20 +1,14 @@
-// swift-tools-version:4.1
+// swift-tools-version:5.0
+
 import PackageDescription
 
 let package = Package(
     name: "MaverickModels",
     products: [
-        .library(
-            name: "MaverickModels",
-            targets: ["MaverickModels"]),
+        .library(name: "MaverickModels", targets: ["MaverickModels"]),
     ],
     dependencies: [],
     targets: [
-        .target(
-            name: "MaverickModels",
-            dependencies: []),
-        .testTarget(
-            name: "MaverickModelsTests",
-            dependencies: ["MaverickModels"]),
+        .target(name: "MaverickModels", dependencies: []),
     ]
 )
--- Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-import maverick_modelsTests
-
-var tests = [XCTestCaseEntry]()
-tests += maverick_modelsTests.allTests()
-XCTMain(tests)
